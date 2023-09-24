9257dbbca995287529d5912f126d9868b85c9147
Ryan Rodemoyer <ryan.rodemoyer@icloud.com>
2023-09-24 01:50:07 +0000

    adding types for another try

--- src/stores/settingsStore.ts
+++ src/stores/settingsStore.ts
@@ -1,11 +1,40 @@
 import { defineStore } from "pinia";
 
+interface ISocials {
+  fb: string;
+  gh: string;
+  ig: string;
+  li: string;
+  x: string;
+  yt: string;
+}
+interface IMedia {
+  profile: string | null;
+  logo: string | null;
+}
+interface ISettingsState {
+  f: {
+    name: string;
+    title: string;
+    company: string;
+    csz: string;
+    email: string;
+    phone: string;
+    website: string;
+    socials: ISocials;
+  };
+  media: IMedia;
+  ui: {
+    font: string;
+  };
+}
+
 // You can name the return value of `defineStore()` anything you want,
 // but it's best to use the name of the store and surround it with `use`
 // and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
 // the first argument is a unique id of the store across your application
 export const useSettingsStore = defineStore("settings", {
-  state: () => ({
+  state: (): ISettingsState => ({
     f: {
       name: "Ryan Rodemoyer",
       title: "Automation Expert",
@@ -24,8 +53,8 @@ export const useSettingsStore = defineStore("settings", {
       },
     },
     media: {
-      // profile: null,
-      // logo: null,
+      profile: null,
+      logo: null,
     },
     ui: {
       font: "eb garamond",
@@ -40,13 +69,11 @@ export const useSettingsStore = defineStore("settings", {
       this.ui.font = name;
     },
     setMediaProfile(e: any) {
-      console.log(e.target.type);
-      const file: File = e.target.files[0];
+      const file = e.target.files[0];
       this.media.profile = URL.createObjectURL(file);
     },
     setMediaLogo(e: any) {
-      console.log(e.target.type);
-      const file: File = e.target.files[0];
+      const file = e.target.files[0];
       this.media.logo = URL.createObjectURL(file);
     },
   },
